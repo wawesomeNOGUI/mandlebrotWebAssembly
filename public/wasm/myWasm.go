3a412791910c9fe87ec4e1e5ff058156743403b1
Will Forcey <wsforc3y@gmail.com>
2022-10-28 17:22:11 -0400

    Add files via upload

--- public/wasm/myWasm.go
+++ public/wasm/myWasm.go
@@ -17,28 +17,35 @@ var imaginaryEnd float64 = js.Global().Get("imaginaryEnd").Float()
 var res float64 = js.Global().Get("res").Float()
 var maxIteration float64 = js.Global().Get("maxIteration").Float()
 
-func drawMandlebrot(this js.Value, args []js.Value) {
-	for i := float64(0); i < width; i += res {
-		for j := float64(0); j < height; j += res {
-			x := 0
-			y := 0
-			xScaled := realStart + (i/width) * (realEnd - realStart)
-			yScaled := imaginaryStart + (j/height) * (imaginaryEnd - imaginaryStart)
-	
-			iteration := 0;
-			for x*x + y*y <= 2*2 && iteration < maxIteration {
-				xTemp := x*x - y*y + xScaled
-				y = 2*x*y + yScaled
-				x = xTemp
-				iteration++
-			}
-	
-			if x*x + y*y > 4.5 {
-				ctx.Set("fillStyle", "#FFFFFF")
-				ctx.Call("fillRect", i, j, res, res)
-			} 
-		}
+var i float64 = js.Global().Get("i").Float()
+var j float64
+func drawMandlebrot(this js.Value, args []js.Value) any {
+	x := float64(0)
+	y := float64(0)
+	xScaled := realStart + (i/width) * (realEnd - realStart)
+	yScaled := imaginaryStart + (j/height) * (imaginaryEnd - imaginaryStart)
+
+	iteration := float64(0);
+	for x*x + y*y <= 2*2 && iteration < maxIteration {
+		xTemp := x*x - y*y + xScaled
+		y = 2*x*y + yScaled
+		x = xTemp
+		iteration++
 	}
+
+	if x*x + y*y >= 4 {
+		ctx.Set("fillStyle", "#FFFFFF")
+		ctx.Call("fillRect", i, j, res, res)
+	} 
+
+	j += res;
+    if(j > height) {
+        j = 0;
+        i += res;
+		js.Global().Set("i", i)
+    }  
+
+	return true
 }
 
 func main() {  
